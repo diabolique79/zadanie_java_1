2f36fc4933b4e4f491a06824f3622f809811e224
Magda Kubin <m.mielnicka79@gmail.com>
2018-07-03 21:27:38 +0200

    Druga wersja programu (po zmianach)

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.print("Uczę\nsię\nprogramowania");
+        System.out.println("Uczę\nsię\nprogramowania");
+        System.out.println("i\ncommitowania");
     }
 }
